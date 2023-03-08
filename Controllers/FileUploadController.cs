9882f76ae611ddd452918881de7cdcc86f7e3630
Rusiru Gunaratne <info.rusiru@gmail.com>
2023-03-08 11:17:42 +0530

    file upload added

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,30 @@
+﻿using APIFarm.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIFarm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUploadController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpPost("[action]")]
+        public IActionResult UploadFiles([FromForm] FileModel fileData)
+        {
+            var file = fileData.file;
+            string directoryPath = Path.Combine(_webHostEnvironment.ContentRootPath, "UploadedFiles");
+            string filePath = Path.Combine(directoryPath, file.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            return Ok(filePath);
+        }
+    }
+}
